1378a0c7fb8c946e0f3ca89ba5b02ee65c80152c
Sodi <tassaduq009@gmail.com>
2024-02-22 05:59:57 +0000

    Working On ResultsPage

--- LudoClient/App.xaml.cs
+++ LudoClient/App.xaml.cs
@@ -10,7 +10,7 @@ namespace LudoClient
 
             //MainPage = new AppShell();
             //MainPage = new DailyBonusPage();
-            MainPage = new PopUpPage();
+            MainPage = new ResultsPage();
             //MainPage = new TabHandeler();
         }
 #if WINDOWS
