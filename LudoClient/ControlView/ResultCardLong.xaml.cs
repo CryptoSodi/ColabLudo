006ff9b9287e6857e1cf338d70d82d69ae8b7017
Sodi <tassaduq009@gmail.com>
2024-02-24 06:07:45 +0000

    result page design updated and some bindable properties added

--- LudoClient/ControlView/ResultCardLong.xaml.cs
+++ LudoClient/ControlView/ResultCardLong.xaml.cs
@@ -2,8 +2,54 @@ namespace LudoClient.ControlView;
 
 public partial class ResultCardLong : ContentView
 {
-	public ResultCardLong()
-	{
-		InitializeComponent();
-	}
+    public BindableProperty BackGroundImageProperty = BindableProperty.Create(nameof(BackGroundImage), typeof(string), typeof(ResultCardLong), propertyChanged: (bindable, oldValue, newValue) =>
+    {
+        var control = (ResultCardLong)bindable;
+        control.BgImageItem.Source = (string)newValue;
+    });
+    public string BackGroundImage
+    {
+        get => GetValue(BackGroundImageProperty) as string;
+        set => SetValue(BackGroundImageProperty, value);
+    }
+
+    public BindableProperty BorderImageProperty = BindableProperty.Create(nameof(BorderImage), typeof(string), typeof(ResultCardLong), propertyChanged: (bindable, oldValue, newValue) =>
+    {
+        var control = (ResultCardLong)bindable;
+        control.BorderImageItem.Source = (string)newValue;
+    });
+    public string BorderImage
+    {
+        get => GetValue(BorderImageProperty) as string;
+        set => SetValue(BorderImageProperty, value);
+    }
+
+    public BindableProperty StarTypeItemProperty = BindableProperty.Create(nameof(StarImage), typeof(string), typeof(ResultCardLong), propertyChanged: (bindable, oldValue, newValue) =>
+    {
+        var control = (ResultCardLong)bindable;
+        control.StarTypeItem.Source = (string)newValue;
+    });
+    public string StarImage
+    {
+        get => GetValue(StarTypeItemProperty) as string;
+        set => SetValue(StarTypeItemProperty, value);
+    }
+
+    public BindableProperty PlayerNameProperty = BindableProperty.Create(nameof(PlayerName), typeof(string), typeof(ResultCardLong), propertyChanged: (bindable, oldValue, newValue) =>
+    {
+        var control = (ResultCardLong)bindable;
+        control.PlayerNameItem.Text = (string)newValue;
+    });
+    public string PlayerName
+    {
+        get => GetValue(PlayerNameProperty) as string;
+        set => SetValue(PlayerNameProperty, value);
+    }
+
+    
+
+    public ResultCardLong()
+    {
+        InitializeComponent();
+    }
 }
