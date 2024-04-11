8bd8365b931ea47c4330e6ea6e51e20643925141
Sodi <tassaduq009@gmail.com>
2024-04-12 00:36:59 +0100

    Collapse Sheet completed images added

---
+++ LudoClient/ControlView/TransactionLongDetailList.xaml.cs
@@ -0,0 +1,25 @@
+namespace LudoClient.ControlView;
+
+public partial class TransactionLongDetailList : ContentView
+{
+	public TransactionLongDetailList()
+	{
+		InitializeComponent();
+	}
+
+    private void Expand_Clicked(object sender, EventArgs e)
+    {
+        if (ExpandSheet.Padding.Top > 0)
+        {
+            ExpandSheet.Padding = new Thickness(0, 0, 0, 0);
+            ExpandSheet.Margin = new Thickness(0, 0, 0, 0);
+            SheetDirection.Source="arr_down.png";
+            return;
+        }
+        else
+        {
+            SheetDirection.Source = "arr_up.png";
+            ExpandSheet.Padding = new Thickness(0, (SubSheet.Height - 10), 0, 0);
+        }
+    }
+}
--- LudoClient/TransactionPage.xaml.cs
+++ LudoClient/TransactionPage.xaml.cs
@@ -27,15 +27,4 @@ public partial class TransactionPage : ContentPage
         Tab5.SwitchSource = Tab5.SwitchOff;
     }
 
-    private void Expand_Clicked(object sender, EventArgs e)
-    {
-        if(ExpandSheet.Padding.Top>0)
-        {
-            ExpandSheet.Padding = new Thickness(0,0,0,0);
-            ExpandSheet.Margin = new Thickness(0,0,0,0);
-            return;
-        }else
-        ExpandSheet.Padding = new Thickness(0,(SubSheet.Height-10),0,0);
-        
-    }
 }
