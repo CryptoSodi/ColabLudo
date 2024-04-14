d00d7222432f9bd60f9464ead47990604ad2607e
Sodi <tassaduq009@gmail.com>
2024-04-14 04:09:57 +0100

    settings page half comepleted

--- LudoClient/GameSettings.xaml.cs
+++ LudoClient/GameSettings.xaml.cs
@@ -1,9 +1,28 @@
+using LudoClient.ControlView;
+using Microsoft.Maui.Controls;
+
 namespace LudoClient;
 
 public partial class GameSettings : ContentPage
 {
 	public GameSettings()
-	{
+    {
 		InitializeComponent();
-	}
+        Tab1.SwitchSource = Tab1.SwitchOn;
+        Tab2.SwitchSource = Tab2.SwitchOff;
+        Tab3.SwitchSource = Tab3.SwitchOff;
+        Tab4.SwitchSource = Tab4.SwitchOff;
+    }
+    private void TabRequestedActivate(object sender, EventArgs e)
+    {
+        ActivateTab(sender as ImageSwitch);
+    }
+    private void ActivateTab(ImageSwitch activeTab)
+    {
+        Tab1.SwitchSource = Tab1 == activeTab ? Tab1.SwitchOn : Tab1.SwitchOff;
+        Tab2.SwitchSource = Tab2 == activeTab ? Tab2.SwitchOn : Tab2.SwitchOff;
+        Tab3.SwitchSource = Tab3 == activeTab ? Tab3.SwitchOn : Tab3.SwitchOff;
+        Tab4.SwitchSource = Tab4 == activeTab ? Tab4.SwitchOn : Tab4.SwitchOff;
+        // Add logic here to change the content based on the active tab
+    }
 }
