293b24580b798c209a32a51e900a02c6999d31de
Sodi <tassaduq009@gmail.com>
2024-04-22 23:39:58 +0100

    Game Board Added

--- LudoClient/App.xaml.cs
+++ LudoClient/App.xaml.cs
@@ -9,7 +9,7 @@ namespace LudoClient
             InitializeComponent();
 
            // MainPage = new AppShell();
-            MainPage = new GameSettingsLarge();
+            MainPage = new Game();
             
             //MainPage = new DashboardPage();
             //MainPage = new TabHandeler();
