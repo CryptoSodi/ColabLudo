82c1d2bdaa28b0c018c14e60666837548a8a5fb5
harisraza9 <harisrazashaikh@live.com>
2024-06-02 18:29:27 +0100

    Google Authentication Android

--- LudoClient/LoginPage.xaml.cs
+++ LudoClient/LoginPage.xaml.cs
@@ -1,5 +1,7 @@
 using LudoClient.Utilities;
 using Microsoft.Maui.Controls;
+using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
 
 namespace LudoClient
 {
@@ -28,13 +30,101 @@ namespace LudoClient
         {
             try
             {
-                // Optionally handle post-sign-in logic here, such as navigating to a new page
+                SemanticScreenReader.Announce(CounterBtn.Text);
+                OnAuthenticate("Google");
+                /*/ Generate state
+                state = GenerateState();
+                string clientId = "497194700135-108a3be43e3kv3oae82pdtl9d1ju9dom.apps.googleusercontent.com";
+                string redirectUri = "http://localhost:5000";
+                string authorizationUrl = $"https://accounts.google.com/o/oauth2/auth?client_id={clientId}&redirect_uri={Uri.EscapeDataString(redirectUri)}&scope=openid%20https://www.googleapis.com/auth/userinfo.email%20profile%20email&response_type=code&access_type=offline&hl=en&state={state}";
+
+
+               // var result = await WebAuthenticator.AuthenticateAsync(new Uri(authorizationUrl), new Uri(redirectUri));
+
+                await Browser.OpenAsync(authorizationUrl, BrowserLaunchMode.SystemPreferred);
+
+                // Wait for redirect and handle auth
+                HandleGoogleAuth();
+            */
             }
             catch (Exception ex)
             {
                 // Handle or log the error
                 await DisplayAlert("Error", "Failed to sign in: " + ex.Message, "OK");
             }
+        }
+
+        private static string GenerateState()
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var byteArray = new byte[32];
+                rng.GetBytes(byteArray);
+                return Convert.ToBase64String(byteArray);
+            }
+        }
+        const string authenticationUrl = "https://xamarin-essentials-auth-sample.azurewebsites.net/mobileauth/";
+        async Task OnAuthenticate(string scheme)
+        {
+            String AuthToken;
+            try
+            {
+                WebAuthenticatorResult r = null;
+
+                if (scheme.Equals("Apple", StringComparison.Ordinal)
+                    && DeviceInfo.Platform == DevicePlatform.iOS
+                    && DeviceInfo.Version.Major >= 13)
+                {
+                    // Make sure to enable Apple Sign In in both the
+                    // entitlements and the provisioning profile.
+                    var options = new AppleSignInAuthenticator.Options
+                    {
+                        IncludeEmailScope = true,
+                        IncludeFullNameScope = true,
+                    };
+                    r = await AppleSignInAuthenticator.AuthenticateAsync(options);
+                }
+                else
+                {
+                    var authUrl = new Uri(authenticationUrl + scheme);
+                    var callbackUrl = new Uri("xamarinessentials://");
+
+                    r = await WebAuthenticator.AuthenticateAsync(authUrl, callbackUrl);
+                }
+
+                AuthToken = string.Empty;
+                if (r.Properties.TryGetValue("name", out var name) && !string.IsNullOrEmpty(name))
+                    AuthToken += $"Name: {name}{Environment.NewLine}";
+                if (r.Properties.TryGetValue("email", out var email) && !string.IsNullOrEmpty(email))
+                    AuthToken += $"Email: {email}{Environment.NewLine}";
+                AuthToken += r?.AccessToken ?? r?.IdToken;
+                GetUserInfoAsync(AuthToken);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Login canceled.");
+
+                AuthToken = string.Empty;
+                DisplayAlert("Error", "Login canceled.", "ok");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed: {ex.Message}");
+
+                AuthToken = string.Empty;
+
+                DisplayAlert("Error", $"Failed: {ex.Message}", "ok");
+            }
+        }
+
+        private static async Task<JObject> GetUserInfoAsync(string accessToken)
+        {
+            var userInfoUrl = "https://www.googleapis.com/oauth2/v1/userinfo";
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await client.GetAsync(userInfoUrl);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return JObject.Parse(responseString);
         }
 
         private void SendOTP_Clicked(object sender, EventArgs e)
--- LudoClient/Platforms/Android/MainActivity.cs
+++ LudoClient/Platforms/Android/MainActivity.cs
@@ -1,11 +1,21 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
 
 namespace LudoClient
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+        [IntentFilter(
+     new[] { Intent.ActionView },
+     Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+     DataScheme = "xamarinessentials")]
+        public class WebAuthenticationCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
+        {
+        }
     }
 }
