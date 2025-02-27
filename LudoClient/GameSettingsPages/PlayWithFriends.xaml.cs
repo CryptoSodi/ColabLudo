4a6cba1f108506575b46ede529fe2ec06a832ee2
Sodi <tassaduq009@gmail.com>
2025-02-27 07:20:29 +0500

    added bet setting functions

--- LudoClient/GameSettingsPages/PlayWithFriends.xaml.cs
+++ LudoClient/GameSettingsPages/PlayWithFriends.xaml.cs
@@ -1,10 +1,16 @@
 using LudoClient.Constants;
 using LudoClient.ControlView;
+using Microsoft.Maui;
+using SharedCode.Constants;
+using System.Threading.Tasks;
 
 namespace LudoClient;
 
 public partial class PlayWithFriends : ContentPage
 {
+    public int entry = GlobalConstants.initialEntry;
+    public int win = GlobalConstants.initialEntry * 2;
+    public bool defaultTabSelection = true;
     public PlayWithFriends()
     {
         InitializeComponent();
@@ -15,14 +21,30 @@ public partial class PlayWithFriends : ContentPage
 
         TabP1.SwitchSource = TabP1.SwitchOn;
         TabP2.SwitchSource = TabP2.SwitchOff;
+        EntryLabel.Text = GlobalConstants.initialEntry.ToString();
+        WinLabel.Text = (GlobalConstants.initialEntry * 2).ToString();
     }
     private void TabRequestedActivate(object sender, EventArgs e)
     {
-        ImageSwitch activeTab = sender as ImageSwitch;
-        Tab1.SwitchSource = Tab1 == activeTab ? Tab1.SwitchOn : Tab1.SwitchOff;
-        Tab2.SwitchSource = Tab2 == activeTab ? Tab2.SwitchOn : Tab2.SwitchOff;
-        Tab3.SwitchSource = Tab3 == activeTab ? Tab3.SwitchOn : Tab3.SwitchOff;
-        Tab4.SwitchSource = Tab4 == activeTab ? Tab4.SwitchOn : Tab4.SwitchOff;
+        ActivateTab(sender as ImageSwitch);
+    }
+    private void ActivateTab(ImageSwitch activeTab)
+    {
+        var configTab1 = Tab1 == activeTab ? (Source: Tab1.SwitchOn, Active: true) : (Source: Tab1.SwitchOff, Active: false);
+        Tab1.SwitchSource = configTab1.Source;
+        Tab1.IsActive = configTab1.Active;
+        var configTab2 = Tab2 == activeTab ? (Source: Tab2.SwitchOn, Active: true) : (Source: Tab2.SwitchOff, Active: false);
+        Tab2.SwitchSource = configTab2.Source;
+        Tab2.IsActive = configTab2.Active;
+        var configTab3 = Tab3 == activeTab ? (Source: Tab3.SwitchOn, Active: true) : (Source: Tab3.SwitchOff, Active: false);
+        Tab3.SwitchSource = configTab3.Source;
+        Tab3.IsActive = configTab3.Active;
+        var configTab4 = Tab4 == activeTab ? (Source: Tab4.SwitchOn, Active: true) : (Source: Tab4.SwitchOff, Active: false);
+        Tab4.SwitchSource = configTab4.Source;
+        Tab4.IsActive = configTab4.Active;
+        // Add logic here to change the content based on the active tab
+        defaultTabSelection = false;
+        CalculateWin();
     }
     private void TabRequestedActivateP(object sender, EventArgs e)
     {
@@ -35,6 +57,7 @@ public partial class PlayWithFriends : ContentPage
             TabCreate.IsVisible = true;
             actionBtn.Source = Skins.CreateBTN; //"btn_create.png";
             //public static string CreateBTN => $"btn_create{CurrentSkinType}.png";
+            ShowWinAmount.IsVisible = true;
         }
         else
         {
@@ -42,10 +65,50 @@ public partial class PlayWithFriends : ContentPage
             TabCreate.IsVisible = false;
             actionBtn.Source = Skins.JoinBTN; //"btn_join_large.png";
             //public static string JoinBTN => $"btn_join_large{CurrentSkinType}.png";
+            ShowWinAmount.IsVisible = false;
         }
     }
+    private void BtnPlus(object sender, EventArgs e)
+    {
+        entry += GlobalConstants.initialEntry;
+        EntryLabel.Text = entry.ToString();
+        CalculateWin();
+    }
+    private void BtnMinus(object sender, EventArgs e)
+    {
+        if (entry > GlobalConstants.initialEntry)
+        {
+            entry -= GlobalConstants.initialEntry;
+            EntryLabel.Text = entry.ToString();
+            CalculateWin();
+        }
+    }
+    private void CalculateWin()
+    {
+        if (Tab1.IsActive || Tab4.IsActive || defaultTabSelection)
+        {
+            win = entry * 2;
+        }
+        else if (Tab2.IsActive)
+        {
+            win = entry * 3;
+        }
+        else if (Tab3.IsActive)
+        {
+            win = entry * 4;
+        }
+        WinLabel.Text = win.ToString();
+    }
+
     private void CreateJoinTapped(object sender, EventArgs e)
     {
-
+    }
+    private async void BtnPaste(object sender, EventArgs e)
+    {
+        if (Clipboard.HasText)
+        {
+            string copiedText = await Clipboard.GetTextAsync();
+            RoomId.entryField.Text = copiedText;
+        }
     }
 }
